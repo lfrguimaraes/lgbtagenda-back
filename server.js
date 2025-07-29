ac79d2a61f3b81585e8c6a96870ac4902622dff4
luiz <lfrguimaraes@gmail.com>
2025-07-29 13:38:21 +0200

    index.js added

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => {
   console.log('✅ MongoDB connected');
-  app.listen(process.env.PORT || 5000, () => {
+  app.listen(process.env.PORT || 5050, () => {
     console.log(`🚀 Server running on port ${process.env.PORT || 5050}`);
   });
 }).catch(err => console.error('❌ MongoDB error:', err));
