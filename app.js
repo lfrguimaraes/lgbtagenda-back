aa6a951d29f34381169bdab5de7d3992e92ee2af
luiz <lfrguimaraes@gmail.com>
2025-07-30 19:50:20 +0200

    update app.js

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ const specs = swaggerJsdoc(options);
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use(cors());
-app.use(express.json()); // for parsing JSON bodies
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.get('/', (req, res) => {
   res.send('LGBT Agenda backend is running!');
